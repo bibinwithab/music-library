131e0e39fe9f32e5ae9922e76e64a53eee1340e7
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-05-17 14:15:28 +0530

    Added MySQL and Pug dependencies, created root route

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const logger = require("./src/middleware/logger");
 const errorHandler = require("./src/middleware/errorHandler");
+const rootRoute = require("./src/routes/root");
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(logger);
 app.use(errorHandler);
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/", rootRoute);
 
 app.listen(PORT, () => {
   console.log(`Server up on http://localhost:${PORT}/`);
