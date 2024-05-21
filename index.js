7cf9845268b951eb28adde3eb50c38db356ec1e3
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-05-21 13:56:51 +0530

    Update dependencies and configure MongoDB connection

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const logger = require("./src/middleware/logger");
 const errorHandler = require("./src/middleware/errorHandler");
 const rootRoute = require("./src/routes/root");
+const dbConfig = require('./src/config/dbConfig')
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(logger);
-app.use(errorHandler);
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -18,6 +18,7 @@ app.use(
 
 app.use("/", rootRoute);
 
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server up on http://localhost:${PORT}/`);
 });
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,12 +1,17 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+const mongoose = require('mongoose')
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const dbConfig = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Error connecting to database", error);
+  }
+}
+
+module.exports = dbConfig;
