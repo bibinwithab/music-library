8f5ae9baee4a0d76501238d78078f664cbf0fbf3
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-05-22 17:32:47 +0530

    updated playlist and song routes

--- server/src/routes/playlistRoute.js
+++ server/src/routes/playlistRoute.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware")
 
-
+router
+  .post("/newPlaylist", authMiddleware, playlistController.createPlaylist)
+  .post("/addSongs", authMiddleware, playlistController.addToPlaylist)
+  .delete("/removeSong", authMiddleware, playlistController.removeFromPlaylist);
 
 module.exports = router;
--- server/src/routes/songRoute.js
+++ server/src/routes/songRoute.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-
+router
+  .post("/addSong", songController.addSong)
+  .delete("/delete/:id", songController.deleteSong)
+  .put("/update/:id", songController.updateSong)
+  .get("/all", songController.getAllSongs)
+  .get("/one/:id", songController.getOneSong)
+  .post("/organize", songController.organizeSongs)
+  .get("/search", songController.searchSong);
 
 module.exports = router;
