cb33309daf0c5f11f36f27519411df6dc2bdeff2
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-05-24 13:48:13 +0530

    updated playlist routes

--- server/src/routes/playlistRoute.js
+++ server/src/routes/playlistRoute.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware")
 const playlistController = require("../controllers/playlistController");
 
-// create route for all playlist
-// create route for all songs in playlist 
 router
   .post("/newPlaylist", authMiddleware, playlistController.createPlaylist)
   .post("/addSongs", authMiddleware, playlistController.addToPlaylist)
