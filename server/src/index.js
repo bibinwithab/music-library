0b2559a20baa0316ea7a62978e65e153f128f9cb
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2025-02-10 10:54:07 +0530

    cors update

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://music-lib-client.vercel.app/',
+  origin: 'https://music-lib-client.vercel.app',
   credentials: true
 }));
 app.use(
